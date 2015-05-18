293b7cfafda7e4b3931a42e8f25ef99c2a702b65
Robert Warren <grindnmosh@gmail.com>
2015-05-18 09:33:59 -0700

    All Screens are done and navigation has begun

---
+++ SPP/app/src/main/java/com/ginddesign/spp/LSignInActivity.java
@@ -0,0 +1,101 @@
+package com.ginddesign.spp;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.LogInCallback;
+import com.parse.ParseUser;
+
+
+public class LSignInActivity extends ActionBarActivity {
+
+    Button signin;
+    String userid;
+    String pass;
+    EditText username;
+    EditText password;
+    Context thisHere = this;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lsignin);
+
+        username = (EditText) findViewById(R.id.username);
+        password = (EditText) findViewById(R.id.password);
+        signin = (Button) findViewById(R.id.sibutt);
+
+        signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                userid = username.getText().toString();
+                pass = password.getText().toString();
+
+                ConnectivityManager cm = (ConnectivityManager) thisHere.getSystemService(Context.CONNECTIVITY_SERVICE);
+                NetworkInfo netInfo = cm.getActiveNetworkInfo();
+                if (netInfo != null && netInfo.isConnectedOrConnecting()) {
+
+
+                    ParseUser.logInInBackground(userid, pass,
+                            new LogInCallback() {
+                                public void done(ParseUser parseUser, com.parse.ParseException e) {
+                                    if (parseUser != null) {
+                                        Intent intent = new Intent(
+                                                LSignInActivity.this,
+                                                LockersActivity.class);
+                                        startActivity(intent);
+                                        Toast.makeText(getApplicationContext(),
+                                                "Successfully Logged in",
+                                                Toast.LENGTH_LONG).show();
+                                        finish();
+                                    } else {
+                                        Toast.makeText(
+                                                getApplicationContext(),
+                                                "No such user exist, please signup",
+                                                Toast.LENGTH_LONG).show();
+                                    }
+                                }
+
+                            });
+
+
+                } else {
+                    Toast.makeText(thisHere, "No network detected. Please connect to a network to login", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_lsignin, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
