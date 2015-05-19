5d34f44727fe7f2f9661bd278bbecb4fe4866462
Robert Warren <grindnmosh@gmail.com>
2015-05-18 19:05:00 -0700

    Updated camera icon for add images in image locker

---
+++ SPP/app/src/main/java/com/ginddesign/spp/LImageActivity.java
@@ -0,0 +1,58 @@
+package com.ginddesign.spp;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.parse.ParseUser;
+import com.parse.ui.ParseLoginBuilder;
+
+
+public class LImageActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_limage);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_limage, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            ParseUser.logOut();
+            ParseLoginBuilder builder = new ParseLoginBuilder(LImageActivity.this);
+            startActivityForResult(builder.build(), 0);
+            finish();
+        }
+        else if (id == R.id.action_qc) {
+            Intent qc = new Intent(this, QuickContactActivity.class);
+            this.startActivity(qc);
+        }
+        else if (id == R.id.action_home) {
+            Intent lock = new Intent(this, MainActivity.class);
+            this.startActivity(lock);
+        }
+        else if (id == R.id.action_add) {
+            Intent add = new Intent(this, LNewActivity.class);
+            this.startActivity(add);
+        }
+
+
+        return super.onOptionsItemSelected(item);
+    }
+}
