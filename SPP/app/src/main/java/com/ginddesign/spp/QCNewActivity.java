05f824e4407d41a5cdf992a68c99b3ce45637c9b
Robert Warren <grindnmosh@gmail.com>
2015-05-20 00:49:19 -0700

    Quick Contacts form now saving to parse with functional spinner.

--- SPP/app/src/main/java/com/ginddesign/spp/QCNewActivity.java
+++ SPP/app/src/main/java/com/ginddesign/spp/QCNewActivity.java
@@ -2,16 +2,23 @@ package com.ginddesign.spp;
 
 import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
+import com.parse.ParseACL;
+import com.parse.ParseObject;
 import com.parse.ParseUser;
 import com.parse.ui.ParseLoginBuilder;
 
@@ -19,6 +26,15 @@ import com.parse.ui.ParseLoginBuilder;
 public class QCNewActivity extends AppCompatActivity {
 
     String[] loads;
+    String cat;
+    String name;
+    String phone;
+    String email;
+    String notes;
+    EditText qcName;
+    EditText qcPhone;
+    EditText qcEmail;
+    EditText qcNotes;
     Button cancel;
     Button save;
     Spinner s;
@@ -33,17 +49,78 @@ public class QCNewActivity extends AppCompatActivity {
 
         save = (Button) findViewById(R.id.save1);
         cancel = (Button) findViewById(R.id.cancel1);
+        qcName = (EditText) findViewById(R.id.qcName);
+        qcPhone = (EditText) findViewById(R.id.qcPhone);
+        qcEmail = (EditText) findViewById(R.id.qcEmail);
+        qcNotes = (EditText) findViewById(R.id.qcNotes);
         s = (Spinner) findViewById(R.id.qcSpin);
 
         loads = getResources().getStringArray(R.array.qc_cat);
         loadsAdapter = new ArrayAdapter<>(context, android.R.layout.simple_spinner_item, android.R.id.text1, loads);
         s.setAdapter(loadsAdapter);
 
+        s.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if (loads[position].equals("Current Lists")) {
+                    cat = loads[position].trim();
+                }
+                else {
+                    cat = loads[position].trim();
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 
         save.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                name = qcName.getText().toString().trim();
+                phone = qcPhone.getText().toString().trim();
+                email = qcEmail.getText().toString().trim();
+                notes = qcNotes.getText().toString().trim();
 
+                if (!cat.equals("Current Lists") && !name.equals("") && !phone.equals("")) {
+                    ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+                    NetworkInfo netInfo = cm.getActiveNetworkInfo();
+                    if (netInfo != null && netInfo.isConnectedOrConnecting()) {
+
+
+                        ParseObject contacts = new ParseObject("contacts");
+                        contacts.put("Cat", cat);
+                        contacts.put("Name", name);
+                        contacts.put("Phone", phone);
+                        contacts.put("Email", email);
+                        contacts.put("Notes", notes);
+                        contacts.setACL(new ParseACL(ParseUser.getCurrentUser()));
+                        //listMaster.pinInBackground();
+                        contacts.saveInBackground();
+                        MainActivity.mainListAdapter.notifyDataSetChanged();
+                        Intent home = new Intent(QCNewActivity.this, QuickContactActivity.class);
+                        startActivity(home);
+
+                    } else {
+                        ParseObject contacts = new ParseObject("contacts");
+                        contacts.put("Cat", cat);
+                        contacts.put("Name", name);
+                        contacts.put("Phone", phone);
+                        contacts.put("Email", email);
+                        contacts.put("Notes", notes);
+                        contacts.setACL(new ParseACL(ParseUser.getCurrentUser()));
+                        //listMaster.pinInBackground();
+                        contacts.saveEventually();
+                        MainActivity.mainListAdapter.notifyDataSetChanged();
+                        Intent home = new Intent(QCNewActivity.this, QuickContactActivity.class);
+                        startActivity(home);
+                    }
+
+                }else{
+                    Toast.makeText(context, "Please fill out all fields before saving", Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
