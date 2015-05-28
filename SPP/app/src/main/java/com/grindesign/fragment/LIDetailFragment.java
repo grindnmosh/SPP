0ff81db384168f8cc0ab8d0285417c02ef9b86a4
Robert Warren <grindnmosh@gmail.com>
2015-05-27 21:14:21 -0700

    attempt to compenate for camera flaw in some devices such as LG and Xperia with their image rotation

--- SPP/app/src/main/java/com/grindesign/fragment/LIDetailFragment.java
+++ SPP/app/src/main/java/com/grindesign/fragment/LIDetailFragment.java
@@ -5,6 +5,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.Uri;
@@ -20,15 +22,18 @@ import android.widget.TextView;
 import com.ginddesign.spp.R;
 import com.parse.GetCallback;
 import com.parse.GetDataCallback;
+import com.parse.ParseException;
 import com.parse.ParseFile;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
 
+import java.io.IOException;
 import java.util.List;
 
 
 public class LIDetailFragment extends Fragment {
 
+    int rotate;
     Context context;
     public static String oid;
 
@@ -62,12 +67,37 @@ public class LIDetailFragment extends Fragment {
                     String imgName = object.getString("Name");
                     TextView nameIt = (TextView) view.findViewById(R.id.textView26);
                     nameIt.setText(imgName);
+                    ExifInterface exif = null;
+                    try {
+                        exif = new ExifInterface(fileObject.getUrl());
+                    } catch (IOException e1) {
+                        e1.printStackTrace();
+                    }
+                    int orientation = exif.getAttributeInt(
+                            ExifInterface.TAG_ORIENTATION,
+                            ExifInterface.ORIENTATION_NORMAL);
+
+                    switch (orientation) {
+                        case ExifInterface.ORIENTATION_ROTATE_270:
+                            rotate = 270;
+                            break;
+                        case ExifInterface.ORIENTATION_ROTATE_180:
+                            rotate = 180;
+                            break;
+                        case ExifInterface.ORIENTATION_ROTATE_90:
+                            rotate = 90;
+                            break;
+                    }
+                    final Matrix matrix = new Matrix();
+                    matrix.postRotate(rotate);
                     fileObject.getDataInBackground(new GetDataCallback() {
                         @Override
                         public void done(byte[] data, com.parse.ParseException e) {
                             if (e == null) {
                                 Log.d("test", "We've got data in data.");
                                 Bitmap bmp = BitmapFactory.decodeByteArray(data, 0, data.length);
+                                bmp = Bitmap.createBitmap(bmp, 0, 0, bmp.getWidth(), bmp.getHeight(),
+                                        matrix, true);
                                 ImageView image = (ImageView) view.findViewById(R.id.imageView2);
                                 image.setImageBitmap(bmp);
                             } else {
@@ -87,12 +117,37 @@ public class LIDetailFragment extends Fragment {
                     String imgName = object.getString("Name");
                     TextView nameIt = (TextView) view.findViewById(R.id.textView26);
                     nameIt.setText(imgName);
+                    ExifInterface exif = null;
+                    try {
+                        exif = new ExifInterface(fileObject.getUrl());
+                    } catch (IOException e1) {
+                        e1.printStackTrace();
+                    }
+                    int orientation = exif.getAttributeInt(
+                            ExifInterface.TAG_ORIENTATION,
+                            ExifInterface.ORIENTATION_NORMAL);
+
+                    switch (orientation) {
+                        case ExifInterface.ORIENTATION_ROTATE_270:
+                            rotate = 270;
+                            break;
+                        case ExifInterface.ORIENTATION_ROTATE_180:
+                            rotate = 180;
+                            break;
+                        case ExifInterface.ORIENTATION_ROTATE_90:
+                            rotate = 90;
+                            break;
+                    }
+                    final Matrix matrix = new Matrix();
+                    matrix.postRotate(rotate);
                     fileObject.getDataInBackground(new GetDataCallback() {
                         @Override
                         public void done(byte[] data, com.parse.ParseException e) {
                             if (e == null) {
                                 Log.d("test", "We've got data in data.");
                                 Bitmap bmp = BitmapFactory.decodeByteArray(data, 0, data.length);
+                                bmp = Bitmap.createBitmap(bmp, 0, 0, bmp.getWidth(), bmp.getHeight(),
+                                        matrix, true);
                                 ImageView image = (ImageView) view.findViewById(R.id.imageView2);
                                 image.setImageBitmap(bmp);
                             } else {
